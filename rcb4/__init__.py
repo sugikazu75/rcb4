d41a2c7f70296d3c0fa6ca73109975e96cfab05c
Iori Yanokura <ab.ioryz@gmail.com>
2023-11-23 14:58:17 +0900

    Add warning for python2

--- rcb4/__init__.py
+++ rcb4/__init__.py
@@ -1,6 +1,11 @@
 # flake8: noqa
 
+import sys
 import pkg_resources
 
+
+if sys.version_info[0] < 3:
+    print("\033[91mThis package is not supported in Python 2.\033[0m")
+    sys.exit(1)
 
 __version__ = pkg_resources.get_distribution("rcb4").version
