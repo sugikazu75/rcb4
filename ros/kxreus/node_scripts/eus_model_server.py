4e163723491bdb1947e25f63fba0a55b6ad4cdce
Iori Yanokura <ab.ioryz@gmail.com>
2024-02-27 23:23:29 +0900

    Refactor EusModelServer to optimize URDF loading and robot model updates

--- ros/kxreus/node_scripts/eus_model_server.py
+++ ros/kxreus/node_scripts/eus_model_server.py
@@ -43,8 +43,23 @@ class EusModelServer(object):
         self.clean_namespace = full_namespace[:last_slash_pos] \
             if last_slash_pos != 0 else ''
 
+    def load_robot_model(self, urdf_path):
+        r = RobotModel()
+        with open(urdf_path, 'r') as f:
+            with no_mesh_load_mode():
+                r.load_urdf_file(f)
+        return r
+
+
     def run(self):
         rate = rospy.Rate(1)
+
+        previous_md5sum = None
+        robot_model = None
+
+        rospack = rospkg.RosPack()
+        kxreus_path = rospack.get_path('kxreus')
+
         while not rospy.is_shutdown():
             rate.sleep()
             urdf = rospy.get_param(
@@ -55,23 +70,19 @@ class EusModelServer(object):
                 with open(tmp_file, "w") as f:
                     f.write(urdf)
                 md5sum = checksum_md5(tmp_file)
+                if previous_md5sum is None or previous_md5sum != md5sum:
+                    robot_model = self.load_robot_model(tmp_file)
+                previous_md5sum = md5sum
 
-                rospack = rospkg.RosPack()
-                kxreus_path = rospack.get_path('kxreus')
                 eus_path = os.path.join(
                     kxreus_path, 'models', '{}.l'.format(md5sum))
+                robot_name = robot_model.urdf_robot_model.name
                 if os.path.exists(eus_path):
                     rospy.set_param(self.clean_namespace + '/eus_robot_name',
                                     robot_name)
                     rospy.set_param(self.clean_namespace + '/eusmodel_hash',
                                     md5sum)
                     continue
-
-                r = RobotModel()
-                with open(tmp_file, 'r') as f:
-                    with no_mesh_load_mode():
-                        r.load_urdf_file(f)
-                robot_name = r.urdf_robot_model.name
 
                 lock_path = eus_path + ".lock"
                 lock = FileLock(lock_path, timeout=10)
