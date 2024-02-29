2872b9c61e576f457dbd9b0635356c9c6475b94a
Iori Yanokura <ab.ioryz@gmail.com>
2024-02-29 12:40:46 +0900

    Download viz model for python kxr interface

--- ros/kxr_controller/scripts/interface.py
+++ ros/kxr_controller/scripts/interface.py
@@ -6,6 +6,7 @@ import argparse
 
 import IPython
 from kxr_controller.kxr_interface import KXRROSRobotInterface
+from kxr_models.download_urdf import download_urdf_mesh_files
 import rospy
 from skrobot.model import RobotModel
 
@@ -20,9 +21,12 @@ def main():
     args = parser.parse_args()
 
     rospy.init_node('kxr_interface', anonymous=True)
+
+    download_urdf_mesh_files()
+
     robot_model = RobotModel()
     robot_model.load_urdf_from_robot_description(
-        args.namespace + '/robot_description')
+        args.namespace + '/robot_description_viz')
     ri = KXRROSRobotInterface(  # NOQA
         robot_model, namespace=args.namespace, controller_timeout=60.0)
 
